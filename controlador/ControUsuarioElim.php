47e9dc894f4c06e355d1e5a6cc793f51692f6618
lonkonao <giovanni.caceres.rubio@gmail.com>
2016-03-21 13:06:49 -0300

    fix

--- controlador/ControUsuarioElim.php
+++ controlador/ControUsuarioElim.php
@@ -1,8 +1,16 @@
 <?php
+require_once '../modelo/Data.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$id=$_POST['id'];
+
+
+
+
+$d = new Data();
+
+
+
+$d->borrarUsuario($id);
+
+
 
