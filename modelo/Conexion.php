b0f0f3b1120e51b62661d98b60cbe09a4bc80741
lonkonao <giovanni.caceres.rubio@gmail.com>
2016-03-24 14:52:49 -0300

    Update Conexion.php

--- modelo/Conexion.php
+++ modelo/Conexion.php
@@ -7,7 +7,7 @@ class Conexion {
     
 
     public function __construct() {
-        $this->con = new mysqli(<!-- jajajaj -->);
+        $this->con = new mysqli("dev.saludcormun.cl", "agenda", "TICS.informatica2016", "agendaCormun");
 
         if (!$this->con) {
             die("Error al conectar: " . mysqli_errno());
