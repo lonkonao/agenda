12e7bcaf02ff54011450bde0a75d9e5d1116257b
lonkonao <giovanni.caceres.rubio@gmail.com>
2016-04-03 19:34:51 -0300

    fix agregar IP

--- controlador/ControIP.php
+++ controlador/ControIP.php
@@ -7,19 +7,14 @@ $ip = $_POST['txtIP'];
 
 
 if (!filter_var($ip, FILTER_VALIDATE_IP) === false) {
-//    $d = new Data();
+  $d = new Data();
 //
-//    $d->insertCentro($centro, $dir, $tele);
+    $d->insertIP($ip);
     
     
     header("location: google.cl");
 } else {
-//    echo '<script language="javascript">';
-//            echo 'alert("Ip No valida")'; 
-//            echo '</script>';
-//   
-//   header("Location: ../vista/admin/mantenedor.php?i=".$ip);
-    
+
    echo '<script language="text/javascript">';
             echo 'alert("IP No Valida"); document.location.href="../vista/admin/mantenedor.php"';
             echo '</script>';
