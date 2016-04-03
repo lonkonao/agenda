cdc9739d102c23cc757cb99e977f1eb16d24c0de
lonkonao <giovanni.caceres.rubio@gmail.com>
2016-04-03 20:25:20 -0300

    fix agregar IP quedo OK

--- controlador/ControIP.php
+++ controlador/ControIP.php
@@ -2,22 +2,24 @@
 
 require '../modelo/Data.php';
 $ip = $_POST['txtIP'];
+$nombre = 'null';
+$box = 0;
+$sector = 0;
+$centro = 0;
+$anexo = 0;
+
 
 
 
 
 if (!filter_var($ip, FILTER_VALIDATE_IP) === false) {
-  $d = new Data();
+    $d = new Data();
 //
     $d->insertIP($ip);
-    
-    
-  
 } else {
-
-   echo '<script language="text/javascript">';
-            echo 'alert("IP No Valida"); document.location.href="../vista/admin/mantenedor.php"';
-            echo '</script>';
+    echo '<script language="javascript">';
+    echo 'alert("La Ip No es Valida ");location.href="../vista/admin/mantenedor.php"';
+    echo '</script>';
 }
 
 
